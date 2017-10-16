56cc594cf50055742846035c9c88b18bd01c7c67
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-16 14:27:08 -0400

    Updated from Brython Server: 10/16/2017 2:27:07 PM

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,24 @@
+#Clay Kynor
+#10/16/17
+#functionDemo.py - learning functions
 
+def hw():
+    print("Hellow, world!")
+
+"""
+hw() #actually runs the function"""
+
+def bigger(num1, num2): #prints which number is bigger
+    if num1 > num2:
+        print(num1)
+    else:
+        print(num2)
+        
+#bigger(13,15)
+#bigger(-10,-192)
+#bigger("Clay", "Pedro")
+
+def slope(x1,y1,x2,y2): #calculate slope
+    print((y2-y1)/(x2-x1))
+    
+slope(1,2,2,4)
