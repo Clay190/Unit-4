b13afcc0579cc5a7a5b7e9f7c2a7471d61e03825
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-18 13:54:39 -0400

    Updated from Brython Server: 10/18/2017 1:54:39 PM

--- distance.py
+++ distance.py
@@ -1,1 +1,12 @@
+#Clay Kynor
+#10/18/17
+#distanceDemo.py - calculates distance
 
+from math import sqrt
+
+def distance(x1,y1,x2,y2):
+    return sqrt((x2-x1)**2 + (y2-y1)**2)
+    
+ans = distance(3,4,-5,2)
+    
+print(ans)
