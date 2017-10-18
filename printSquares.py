cbccad05c304b71d3579d90c4003482243b44e65
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-18 13:45:01 -0400

    Updated from Brython Server: 10/18/2017 1:45:00 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#10/18/17
+#printSquares.py
 
+def squares(num2,num1):
+    for i in range(1,(num2+1)):
+        print("+--"*num1+"+")
+        print("|  "*num1+"|")
+    print("+--"*num1+"+")
+
+squares(2,4)
