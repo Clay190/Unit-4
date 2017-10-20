1e2b2c3bf028926b97e129669964398090e5e626
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-20 14:54:24 -0400

    Updated from Brython Server: 10/20/2017 2:54:24 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,13 @@
+#Clay Kynor
+#10/20/17
+#stringUnion.py
 
+def stringIntersect(word1,word2):
+    total = " "
+    for ch in word2:
+        if ch in word1:
+            total += ch
+    return total
+    
+var = stringIntersect("Mississippi","Pennsylvania")
+print(var)
