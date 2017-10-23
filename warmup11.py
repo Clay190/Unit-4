9816dd9cfd404f93a10fcd7eabb9cbee44429ea1
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-23 14:34:56 -0400

    Updated from Brython Server: 10/23/2017 2:34:56 PM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,12 @@
+#Clay Kynor
+#10/23/17
+#warmup11.py - determinds prime numbers
 
+def isPrime(num1):
+    for i in range(2,(num1)):
+        if num1%i==0:
+            return print("False")
+        else:
+            return print("True")
+
+isPrime(3)
