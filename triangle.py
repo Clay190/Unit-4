2fdb5e6815d9f9f62f0a676dc67fdf006d2d5468
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-23 14:55:31 -0400

    Updated from Brython Server: 10/23/2017 2:55:30 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,16 @@
+#Clay Kynor
+#10/23/17
+#traingle.py - find out the areadof a triangle
 
+from math import sqrt
+
+def distance(x1,y1,x2,y2,x3,y3):
+    return sqrt
+    a = sqrt((x2-x1)**2 + (y2-y1)**2)
+    b = sqrt((x3-x2)**2 + (y3-y2)**2)
+    c = sqrt((x1-x3)**2 + (y1-y3)**2)
+    
+    
+ans = distance(1,2,3,4,5,6)
+    
+print(ans)
