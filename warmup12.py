490d238f0247f473e55830e6cc09e6f252da4c61
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-27 14:33:58 -0400

    Updated from Brython Server: 10/27/2017 2:33:55 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,15 @@
+#Clay Kynor
+#10/27/17
+#warmup12.py
 
+def calc(x,y):
+    n=y
+    while n>0:
+        if x%n == 0 and y%n == 0:
+            print(n)
+            break
+        else:
+            n=-1
+            
+            
+defcalc(10,5)
