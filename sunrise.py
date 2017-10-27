46a407407b5505f74fcda726acbba7998ae8e916
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-27 14:52:02 -0400

    Updated from Brython Server: 10/27/2017 2:52:00 PM

--- sunrise.py
+++ sunrise.py
@@ -2,9 +2,59 @@
 #10/27/17
 #sunrise.py
 
-yellow
+from ggame import *
+
+ROWS = 26
+COLS = 50
+CELL_SIZE = 20
+
+def moveRight(event):
+    if monkey.x < (COLS-1)*CELL_SIZE:
+        monkey.x += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+
+def moveLeft(event):
+    if monkey.x > 0:
+        monkey.x -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+    
+def moveUp(event):
+    if monkey.y > 0:
+        monkey.y -= CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+
+def moveDown(event):
+    if monkey.y < (ROWS-1)*CELL_SIZE: 
+        monkey.y += CELL_SIZE
+        if monkey.x == banana.x and monkey.y == banana.y:
+            moveBanana()
+            updateScore()
+yellow = Color(0x00FF00)
+
+def moveBanana():
+    banana.x = randint(0,COLS-1)*CELL_SIZE
+    banana.y = randint(0,ROWS-1)*CELL_SIZE
+    data['frames'] = 0
+    
+def step():
+    data['frames'] += 1
+    if data['frames'] == 149:
+        moveBanana()
+
+yellow = Color(0xFFFF00,1)
 
 
+Sprite(jungleBox)
+monkey = Sprite(sun)
 
-
+App().listenKeyEvent('keydown','right arrow',moveRight)
+App().listenKeyEvent('keydown','left arrow',moveLeft)
+App().listenKeyEvent('keydown','up arrow',moveUp)
+App().listenKeyEvent('keydown','down arrow',moveDown)    
 App().Run()
