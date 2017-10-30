045bfbb8b37be088e98b492b43391d9087be3fda
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-10-30 14:29:04 -0400

    Updated from Brython Server: 10/30/2017 2:29:04 PM

--- quiz4.py
+++ quiz4.py
@@ -5,8 +5,21 @@
 def csia():
     print("Computer Science is Awesome"*5)
 
+def average(num1,num2,num3):
+    return (num1+num2+num3)/3
 
-    
-    
+def lastLetter(word):
+    for ch in word:
+        total = ch
+    print(total)
+
+def same(number1,number2):
+    if number1 == number2:
+        return True
+    else:
+        return False
     
 csia()
+print(average(1,2,3))
+lastLetter('Smedinghoff')
+print(same(2*3,7-1))
